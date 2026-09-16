4f23e3c144418ec4fe3677dcfd545f20fe5ae4a2
Jonghwa <whdghk3471@gmail.com>
2026-09-16 12:59:19 +0900

    #7.2 Data Flow

--- Home.py
+++ Home.py
@@ -1,8 +1,21 @@
 import streamlit as st
-from langchain.prompts import PromptTemplate
+from datetime import datetime
 
-st.write("hello")
-st.write([1, 2, 3, 4])
-st.write(PromptTemplate)
+today = datetime.today().strftime("%H:%M:%S")
 
-st.selectbox("Choose your model", ("GPT-3", "GPT-4"))
+st.title(today)
+
+model = st.selectbox("Choose your model", ("GPT-3", "GPT-4"))
+
+if model == "GPT-3":
+    st.write("cheap")
+else:
+    st.write("not cheap")
+
+    st.write(model)
+
+    name = st.text_input("What is your name?")
+    name
+
+    value = st.slider("temperature", min_value=0.1, max_value=1.0)
+    value

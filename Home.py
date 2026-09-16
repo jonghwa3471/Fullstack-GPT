db571fd2a5e060c71602c36db62c376c2665eefa
Jonghwa <whdghk3471@gmail.com>
2026-09-16 13:46:50 +0900

    #7.3 Multi Page

--- Home.py
+++ Home.py
@@ -1,21 +1,23 @@
 import streamlit as st
-from datetime import datetime
 
-today = datetime.today().strftime("%H:%M:%S")
+st.set_page_config(
+    page_title="FullstackGPT Home",
+    page_icon="🤖",
+)
 
-st.title(today)
-
-model = st.selectbox("Choose your model", ("GPT-3", "GPT-4"))
-
-if model == "GPT-3":
-    st.write("cheap")
-else:
-    st.write("not cheap")
-
-    st.write(model)
-
-    name = st.text_input("What is your name?")
-    name
-
-    value = st.slider("temperature", min_value=0.1, max_value=1.0)
-    value
+st.markdown(
+    """
+# Hello!
+            
+Welcome to my FullstackGPT Portfolio!
+            
+Here are the apps I made:
+            
+- [ ] [DocumentGPT](/DocumentGPT)
+- [ ] [PrivateGPT](/PrivateGPT)
+- [ ] [QuizGPT](/QuizGPT)
+- [ ] [SiteGPT](/SiteGPT)
+- [ ] [MeetingGPT](/MeetingGPT)
+- [ ] [InvestorGPT](/InvestorGPT)
+"""
+)
